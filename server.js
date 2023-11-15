331e38699a299972afe0bb3cc2b1568f380450c1
Negatseg <tseganehnegash@gmail.com>
2023-11-15 17:07:16 -0600

    Clear up the code

--- server.js
+++ server.js
@@ -3,30 +3,12 @@ const mysql = require('mysql2');
 const readline = require('readline');
 const db = require("./db");
 
-// Create a MySQL connection
-// const connection = mysql.createConnection({
-//   host: '127.0.0.1',
-//   user: 'root',
-//   password: 'Texas@4040',
-//   database: 'employee_db',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
 
-// Connect to the database
-// connection.connect((err) => {
-//   if (err) throw err;
-//   console.log('Connected to the database.');
-// });
 
-// Inquirer prompts and logic for user interaction
+n
 async function runApplication() {
   try {
-    // Connect to the database
-    // const connection = await pool.getConnection();
-
-    // Inquirer prompts and logic for user interaction
+    
     const userChoice = await inquirer.prompt({
       type: 'list',
       name: 'action',
@@ -43,7 +25,6 @@ async function runApplication() {
       ],
     });
 
-    // Perform actions based on user choice
     switch (userChoice.action) {
       case 'View all departments':
         viewAllDepartments();
@@ -74,29 +55,24 @@ async function runApplication() {
         break;
 
       case 'Exit':
-        // Close the database connection and exit the program
+        
         connection.release();
         process.exit();
         break;
     }
 
-    // Close the database connection after each user action
-    //connection.release();
+    
   } catch (error) {
     console.error('Error:', error.message);
   } finally {
-    // Continue running the application
     runApplication();
   }
 }
 
-// Start the application
+
 runApplication();
 
-// Display functions for viewing data
 
-// Functions for CRUD operations
-// Example update query
 const updateEmployeeRole = (employeeId, newRoleId) => {
   const query = 'UPDATE employees SET role_id = ? WHERE id = ?';
   connection.query(query, [newRoleId, employeeId], (err, results) => {
@@ -105,38 +81,9 @@ const updateEmployeeRole = (employeeId, newRoleId) => {
   });
 };
 
-// Close the database connection when the program ends
-// connection.end((err) => {
-//   if (err) throw err;
-//   console.log('Connection closed.');
-// });
-
-// Query to retrieve all departments
-//const query = 'SELECT * FROM department';
-
-// Execute the query
-// connection.query(query, (err, results) => {
-//   if (err) {
-//     console.error('Error executing query: ', err);
-//     return;
-//   }
-
-//   // Display the results
-//   console.log('All Departments:');
-//   console.table(results);
-
-//   // Close the connection
-//   connection.end();
-// });
 
 
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-
-// Function to add a department
 function addDepartment() {
   rl.question('Enter department name: ', (departmentName) => {
     const sql = 'INSERT INTO departments (name) VALUES (?)';
@@ -144,21 +91,20 @@ function addDepartment() {
       if (err) throw err;
       console.log('Department added successfully!');
       rl.close();
-      connection.end(); // Close the database connection
+      connection.end(); 
     });
   });
 }
 
-// Function to add a role
+
 function addRole() {
-  // You can add more prompts here to get role-related information
   rl.question('Enter role title: ', (roleTitle) => {
     const sql = 'INSERT INTO roles (title) VALUES (?)';
     connection.query(sql, [roleTitle], (err, results) => {
       if (err) throw err;
       console.log('Role added successfully!');
       rl.close();
-      connection.end(); // Close the database connection
+      connection.end(); 
     });
   });
 }
@@ -173,19 +119,3 @@ function viewAllDepartments(){
   })
 }
 
-// Handle different cases
-// switch (userInput) {
-//   case 'Add a department':
-//     addDepartment();
-//     break;
-
-//   case 'Add a role':
-//     addRole();
-//     break;
-
-//   default:
-//     console.log('Invalid option');
-//     rl.close();
-//     connection.end(); // Close the database connection
-//     break;
-// }
